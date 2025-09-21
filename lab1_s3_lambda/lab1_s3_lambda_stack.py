e2dd6e2e36f9e0c29f4401a5b1f50f22de261a80
ravi <ravi@gmail.com>
2025-09-21 03:52:06 +0000

    lab1_s3_lambda_stack

--- lab1_s3_lambda/lab1_s3_lambda_stack.py
+++ lab1_s3_lambda/lab1_s3_lambda_stack.py
@@ -1,19 +1,36 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    RemovalPolicy,
+    aws_s3 as s3,
+    aws_lambda as _lambda,
+    aws_s3_notifications as s3n,
 )
 from constructs import Construct
+
 
 class Lab1S3LambdaStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # S3 Bucket
+        bucket = s3.Bucket(
+            self, "MyBucket",
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "Lab1S3LambdaQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Lambda Function
+        fn = _lambda.Function(
+            self, "MyLambda",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler="lambda_function.handler",
+            code=_lambda.Code.from_asset("lambda")
+        )
+
+        # Add S3 -> Lambda notification
+        bucket.add_event_notification(
+            s3.EventType.OBJECT_CREATED,
+            s3n.LambdaDestination(fn)
+        )
+
---
+++ lambda/lambda_function.py
@@ -0,0 +1,4 @@
+def handler(event, context):
+    print("Event:", event)
+    return {"statusCode": 200, "body": "Hello from Lambda"}
+
