33ae444d500161c64a47d2c5dadb573558b3e751
EC2 Default User <ec2-user@ip-172-31-5-101.ap-south-1.compute.internal>
2025-09-24 05:48:50 +0000

    update lab2.sh

--- lab2.sh
+++ lab2.sh
@@ -1,14 +1,16 @@
 mkdir lab1-s3-lambda-$1 && cd lab1-s3-lambda-$1
 cdk init app --language python
 
-pip install -r requirements.txt
-pip install aws-cdk-lib constructs
+#pip install -r requirements.txt
+#pip install aws-cdk-lib constructs
 cd ..
 git clone https://github.com/cloud-dev-user/lab1-s3-lambda.git
 cd lab1-s3-lambda
-cp lab1_s3_lambda/lab1_s3_lambda_stack.py  ../lab1-s3-lambda-$1/lab1_s3_lambda_$1/lab1_s3_lambda_$1_stack.py
+cp lab1_s3_lambda/lab1_s3_lambda_stack.py  ../lab1_s3_lambda$1/lab1_s3_lambda_$1_stack.py
 cp -r lambda  ../lab1-s3-lambda-$1/
 cd  ../lab1-s3-lambda-$1/
-source .venv/bin/activate 
+source .venv/bin/activate
+pip install -r requirements.txt
+pip install aws-cdk-lib constructs
 cdk synth
 cdk deploy
